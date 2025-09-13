fbee157c1392233891b1a9a679a7fcd1c18d6f01
Steven Alejandro Suarez Castro <steven@Stevens-MacBook-Pro.local>
2025-09-13 00:58:40 -0500

    update folder structures

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import HomePage from './components/home/HomePage';
 import CreateContractForm from './components/CreateContractForm';
 import ViewContracts from './components/ViewContracts';
 import ContractDetail from './components/ContractDetails';
